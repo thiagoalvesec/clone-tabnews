de4eef96d949c9a5b3f7da8923847abe0b4c93d2
Thiago Alves <thiago.alves.ec@gmail.com>
2024-12-22 08:34:59 -0300

    Adiciona scripts `lint:check e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Recriação do tabnews afim de estudo do cursodev do felipe deschamps, afim de treinar programação.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Ainda não trouxe a coquinha zero :( </h1>
+  return <h1>Ainda não trouxe a coquinha zero :( </h1>;
 }
 
 export default Home;
